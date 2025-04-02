98c58a759d4ac8d85a273a77ff6eb1b474139075
michelsoler74 <michelyborja@gmail.com>
2025-04-02 21:36:15 +0200

    fix: simplificada configuración de Next.js al mínimo

--- next.config.js
+++ next.config.js
@@ -2,19 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [
-      "placehold.co",
-      "avatars.githubusercontent.com",
-      "raw.githubusercontent.com",
-      "github.com",
-      "images.unsplash.com",
-    ],
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**",
+        hostname: "*",
       },
     ],
   },
